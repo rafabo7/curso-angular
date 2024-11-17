f4bdaecf9e1b331495223ce6c5a1cd22d738c8d3
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-11-17 08:51:02 +0100

    Update 02-object-interface

--- 01-intro-typescript/src/topics/02-object-interface.ts
+++ 01-intro-typescript/src/topics/02-object-interface.ts
@@ -5,16 +5,16 @@
 //* Recomended, seriously.
 const skills: string[] = ['Hit', 'Heal', 'Dash']
 // for array of srings => string[]
-// I assume that number[] boolean[] will work too
+// I assume that number[] will work too, boolean[] should work too but don't do that
 
-// modifyng const still working
+// modifyng const still working is TS
 skills.push('Hit harder')
 
 console.log(skills);
 
 //* How to type an object literal
 
-// First create an interface, typescript feature
+// First create an interface, typescript feature, this will type the props of the future object, is not a class, sort of a constructor
 interface Character {
     name: string;
     hp: number;
@@ -23,7 +23,7 @@ interface Character {
     hometown?: string;
 }
 
-// Then declare an object literal typed with the interface
+// Then declare an object literal typed with the interface, it will throw an error if types don`t match
 const strider: Character = {
     name: 'Strider',
     hp: 95,
@@ -37,7 +37,7 @@ console.table(strider)
 
 // Personal doubt
 //? Does Character[] will work for an array of Characters type objects?
-// Later in the lesson it was confirmed.
+// Later in the lesson it was confirmed: an array of character would typed with Character[], later in the course we'll look at ir
 
 
 export {};
