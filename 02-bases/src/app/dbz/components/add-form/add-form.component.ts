f6328735d1809428c034142549c3cd6c8c5e4782
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2025-02-17 16:27:07 +0100

    add: structure for dbz/main-page.component. extract list and form to components in dbz/components/list & ././form

---
+++ 02-bases/src/app/dbz/components/add-form/add-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './add-form.component.html',
+  styleUrl: './add-form.component.css'
+})
+export class AddCharacterComponent {
+
+}
