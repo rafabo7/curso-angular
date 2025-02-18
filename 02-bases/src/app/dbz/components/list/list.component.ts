9084aa267d116e3b2e727b385b83265178f3e871
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2025-02-18 09:09:07 +0100

    update: extract characters list to component logic

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = []
 
 }
