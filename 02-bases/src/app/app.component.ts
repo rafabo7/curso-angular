567dae7f3addbf476083cdbcbcf5122d40b18835
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2025-03-03 17:08:58 +0100

    add: shared component navbar

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './componets/shared/navbar/navbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
