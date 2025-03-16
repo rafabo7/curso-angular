4a9a4ce8c0957a7fe311072874cfcb87cd511475
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2025-03-16 19:32:55 +0100

    add: copy of DragonballPage called DragonballSuper add route, add to navbar, prepare class for next lessons

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page/hero-page.component';
 import { DragonBallPage } from './pages/dragonball/dragonball-page.component';
+import { DragonBallSuperPageComponent } from './pages/dragonball-super/dragonball-super-page.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'dragonball',
     component: DragonBallPage
+  },
+  {
+    path: 'dragonball-super',
+    component: DragonBallSuperPageComponent
   },
   {
     path: '**',
--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface Character {
   id:number,
@@ -7,19 +7,18 @@ interface Character {
 }
 
 @Component({
-  templateUrl: './dragonball-page.component.html'
+  templateUrl: './dragonball-super-page.component.html',
+  selector: 'dragon-ball-super'
 })
 
-export class DragonBallPage {
+export class DragonBallSuperPageComponent {
 
-  name = signal('')
-  power = signal(0)
+  name = signal('Gohan')
+  power = signal(1000)
 
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001},
-    // {id: 2, name: 'Vegeta', power: 8000},
-    // {id: 3, name: 'Krillin', power: 2000},
-    // {id: 4, name: 'Yamcha', power: 500},
+    {id: 2, name: 'Vegeta', power: 8000}
   ])
 
   addCharacter () {
@@ -39,12 +38,5 @@ export class DragonBallPage {
     this.name.set('')
     this.power.set(0)
   }
-
-  powerClasses = computed( () => {
-    // Esto serviria para hacerlo con ngClass
-    return {
-      'text-danger'  : true
-    }
-  })
 
 }
