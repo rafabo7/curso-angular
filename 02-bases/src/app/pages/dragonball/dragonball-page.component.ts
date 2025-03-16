522f62e4b8bc8ca11cdd7732093e303f02922a43
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2025-03-16 19:15:47 +0100

    add: addCharacter method using signals

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -12,12 +12,33 @@ interface Character {
 
 export class DragonBallPage {
 
+  name = signal('Gohan')
+  power = signal(1000)
+
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001},
     {id: 2, name: 'Vegeta', power: 8000},
     {id: 3, name: 'Krillin', power: 2000},
     {id: 4, name: 'Yamcha', power: 500},
   ])
+
+  addCharacter () {
+    if ( !this.name() || !this.power() || this.power() <= 0 ) return
+    const newCharacter: Character = {
+      id: this.characters().length+1,
+      name: this.name(),
+      power: this.power()
+
+    }
+
+    this.characters.update( (current) => [...current, newCharacter] )
+    this.resetInputs()
+  }
+
+  resetInputs() {
+    this.name.set('')
+    this.power.set(0)
+  }
 
   powerClasses = computed( () => {
     // Esto serviria para hacerlo con ngClass
