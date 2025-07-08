194a688532b8f983877157b2bfb8ed71a6ff8430
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2025-07-08 17:32:15 +0200

    refactor side-menu-options clean template create MenuOptions interface declare nav options into menuOptions array replace hardcoded html with menuOptions items info

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  sublabel: string;
+}
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
   templateUrl: './gifs-side-menu-options.component.html'
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent { 
+
+  menuOptions: MenuOption[] = [
+
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Popular Gifs',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      sublabel: 'Browse Gifs',
+      route: '/dashboard/search'
+    }
+
+  ]; 
+
+}
