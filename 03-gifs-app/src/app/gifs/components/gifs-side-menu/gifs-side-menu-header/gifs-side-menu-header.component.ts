6890c1db19308cc2a59bbf5ea49a54ad84862468
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2025-07-08 17:27:38 +0200

    refactor side-menu-header replace template content with data from environment

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,8 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './gifs-side-menu-header.component.html'
 })
-export class GifsSideMenuHeaderComponent { }
+export class GifsSideMenuHeaderComponent { 
+
+  envs = environment
+
+}
