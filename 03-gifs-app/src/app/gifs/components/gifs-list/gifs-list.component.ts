25b1082898fc7f6e82819198272b8410460f843b
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2025-07-09 16:53:32 +0200

    feat (structure an mock ui for trending page) create components for trending-page: gif-list gif-list-item add list of mock url image, prop drill to gif-list iterate for gif-list-item display PS add padding to parent in dashboard.page template

---
+++ 03-gifs-app/src/app/gifs/components/gifs-list/gifs-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
+
+@Component({
+  selector: 'gifs-list',
+  imports: [GifListItemComponent],
+  templateUrl: './gifs-list.component.html'
+})
+export class GifsListComponent { 
+
+
+  gifs = input.required<string[]>()
+
+}
