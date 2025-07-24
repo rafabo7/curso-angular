142553607b29e6074cbd0d106942b3cadce6e9b9
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2025-07-24 19:03:11 +0200

    feat (structure and style) -set home page and footer with hero component from daisyui -structure app.html to always display footer at the bottom

--- 04-geo-app/src/app/app.ts
+++ 04-geo-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppFooterComponent } from "./shared/components/app-footer/app-footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppFooterComponent],
   templateUrl: './app.html'
 })
 export class App {
